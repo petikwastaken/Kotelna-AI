46e997d0195270413a96c816788686ee34bbd582
petikwastaken <82884929+petikwastaken@users.noreply.github.com>
2024-02-17 12:59:11 +0100

    Update ETL.py

--- ETL.py
+++ ETL.py
@@ -2,8 +2,9 @@ from urllib.request import urlopen
 import time as t
 while True:
     print("Fetching data...")
-    page = urlopen("http://213.108.160.85:666/")
+    page = urlopen("http://sampleadr.ess:port")
     print("Formatting data...")
+    #uprava html, tak aby nam zbyla jen teplota na kotli
     page = page.read()
     page = str(page)
     page = page.split(' ')
@@ -11,8 +12,8 @@ while True:
     page = str(page)
     print(page)
     print("Writting data...")
-    soubor = "train-data.txt"   # Přidání textu na konec souboru
-    text_k_pridani = page   # Text, který chcete přidat
+    soubor = "train-data.txt" 
+    text_k_pridani = page 
     with open(soubor, "a") as soubor_objekt: # Otevření souboru v režimu "append" (přidávání)
         soubor_objekt.write(text_k_pridani + "\n")    # Přidání textu na konec souboru
     print("Waiting for 10 minutes...")
